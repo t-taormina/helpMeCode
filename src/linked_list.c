6cb9cdb83002c85826ab604f78d139ec8a607eba
tylr <tylr@debian-BULLSEYE-live-builder-AMD64>
2023-06-17 19:56:12 -0700

    adding initial function headers

--- src/linked_list.c
+++ src/linked_list.c
@@ -1,4 +1,31 @@
 /* Tyler Taormina - taormina@pdx.edu */
 
+#include "linked_list.h"
+
+int 
+add(Program_s *new_prog)
+{
+}
+
+int 
+build_struct(char *prog_name)
+{
+}
+
+int 
+remove(int id_to_remove)
+{
+}
+
+/* File write and read related functions */
+int 
+write_list()
+{
+}
+
+int 
+read_list()
+{
+}
 
 
--- src/linked_list.h
+++ src/linked_list.h
@@ -12,14 +12,25 @@
 #include <sys/time.h>
 
 typedef struct Program_s {
-    char *path;
-    uint32_t program_type_flags;
+    uint32_t  id;
+    char      *name
+    char      *path;
+    uint32_t  program_type_flags;
+    Program_t *next;
 } Program_t;
 
+typedef struct Program_List_s {
+    struct Program_s *head;
+    int              count;
+} Program_List_t;
 
-typedef 
+int add(Program_s *new_prog);
+int build_struct(char *prog_name);
+int remove(int id_to_remove);
 
-
+/* File write and read related functions */
+int write_list();
+int read_list();
 
 #endif // _LINKED_LIST_H
 
