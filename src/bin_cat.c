79a7ea7a9808ff7c3a775af4ae0c5bdb3977f996
tylr <tktaormina@gmail.com>
2023-06-18 11:58:28 -0700

    working write to binary file

---
+++ src/bin_cat.c
@@ -0,0 +1,32 @@
+/* Tyler Taormina */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "linked_list.h"
+
+int
+main(void)
+{
+    program_t temp = {0, "", "", 0};
+    int ifd = -1;
+
+    ifd = open(FILE_NAME, O_RDONLY);
+    if (ifd < 0) {
+        perror("Can't open " FILE_NAME " for reading\n");
+        exit(EXIT_FAILURE);
+    }
+    while(read(ifd, &temp, sizeof(program_t)) > 0) {
+        printf("ID: %d\n\tName of program: %s\n\tPath to program: %s\n"
+                , temp.id, temp.name, temp.path);
+    }
+    close(ifd);
+
+    return EXIT_SUCCESS;
+}
--- src/linked_list.c
+++ src/linked_list.c
@@ -2,32 +2,54 @@
 
 #include "linked_list.h"
 
-#define OPTIONS "hvp:"
+unsigned short is_verbose = 0;
+unsigned short id_global = 1;
+
+int
+run_process(void)
+{
+    program_list_t *program_list = NULL;
+    program_list = (program_list_t *) calloc(1, sizeof(program_list_t));
+    read_list(program_list);
+    for ( ; ; ) {
+        break;
+    }
+    write_list();
+    free_list(program_list);
+    return 0;
+}
+
 
 int 
-process_user_input(int argc, char **argv)
+process_cmdline(int argc, char **argv)
 {
     int opt;
+    char buf[MAX_PATH_LEN];
 
     while((opt = getopt(argc, argv, OPTIONS)) != -1) {
         switch(opt) {
             case 'h':
+                printf("help message here\n");
                 break;
 
             case 'v':
+                is_verbose = !is_verbose;
+                printf("Verbose mode enabled\n");
                 break;
 
             case 'p':
+
                 break;
 
             default:
                 break;
         }
     }
+    return is_verbose;
 }
 
 int 
-insert(Program_t *new_prog)
+insert(program_t *new_prog, program_list_t *program_list)
 {
     if (NULL == new_prog) {
         return -1;
@@ -46,31 +68,54 @@ insert(Program_t *new_prog)
 int 
 build_struct(char *prog_name)
 {
+    return 0;
 }
 
 int 
 remove_program(int id_to_remove)
 {
-}
-
-/* File write and read related functions */
-int 
-write_list()
-{
+    return 0;
 }
 
 int 
-read_list()
+write_list(void)
 {
+    int ofd = -1;
+    program_t init_prog = {
+        id_global
+        , "helpme"
+        , "/home/tylr/Development/Random/helpMeCode/src"
+        , 0 };
+
+    ofd = open(FILE_NAME
+            , O_WRONLY | O_TRUNC | O_CREAT
+            , S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+    if (ofd < 0) {
+        perror("can't open " FILE_NAME " for output\n");
+        exit(EXIT_FAILURE);
+    }
+
+    write(ofd, &init_prog, sizeof(program_t));
+    close(ofd);
+    return 0;
 }
 
 int 
-free_list(Program_List_t *list)
+read_list(program_list_t *prog_list)
 {
+    return 0;
+}
+
+int 
+free_list(program_list_t *list)
+{
+    int count;
+    program_node_t *temp = NULL;
+
     if (NULL == list) { return 0; }
 
-    int count = 0;
-    Program_t *temp = NULL;
+    count = 0;
     while (NULL != list->head) {
         temp = list->head;
         list->head = list->head->next;
@@ -82,5 +127,3 @@ free_list(Program_List_t *list)
     list = NULL;
     return count;
 }
-
-
